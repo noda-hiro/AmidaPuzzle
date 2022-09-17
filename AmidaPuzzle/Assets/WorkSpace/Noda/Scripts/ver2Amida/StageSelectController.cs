dfbb53acf49c23e70387af8a45ef6775ff6d3a03
noda-hiro <hiro13424@gmail.com>
2022-09-17 13:47:49 +0900

    feat : 遊び方画面の追加

--- AmidaPuzzle/Assets/WorkSpace/Noda/Scripts/ver2Amida/StageSelectController.cs
+++ AmidaPuzzle/Assets/WorkSpace/Noda/Scripts/ver2Amida/StageSelectController.cs
@@ -8,24 +8,32 @@ public class StageSelectController : MonoBehaviour
 {
     [SerializeField] private List<Button> ButtonList = new List<Button>();
     [SerializeField] private Button titleButton;
+    [SerializeField] private Button howToPlayButton;
+    [SerializeField] private Button backlSelectButton;
+    [SerializeField] private GameObject howToPlayPanel;
+    private string Stage = "Stage";
+
     // Start is called before the first frame update
     void Start()
     {
         StageBtnSetting();
         OtherButtonSetting();
+        howToPlayButton.onClick.AddListener(() => howToPlayPanel.SetActive(true));
+        backlSelectButton.onClick.AddListener(() => howToPlayPanel.SetActive(false));
     }
     private void StageBtnSetting()
     {
-        ButtonList[0].onClick.AddListener(() => SceneManager.LoadScene("AmidaStage1"));
-        ButtonList[1].onClick.AddListener(() => SceneManager.LoadScene("AmidaStage2"));
-        ButtonList[2].onClick.AddListener(() => SceneManager.LoadScene("AmidaStage3"));
-        //ButtonList[3].onClick.AddListener(() => SceneManager.LoadScene("AmidaStageFour"));
-        //ButtonList[4].onClick.AddListener(() => SceneManager.LoadScene("AmidaStageFive"));
-        ButtonList[5].onClick.AddListener(() => SceneManager.LoadScene("AmidaPuzzle"));
+        ButtonList[0].onClick.AddListener(() => SceneManager.LoadScene(Stage + 1));
+        ButtonList[1].onClick.AddListener(() => SceneManager.LoadScene(Stage + 2));
+        ButtonList[2].onClick.AddListener(() => SceneManager.LoadScene(Stage + 3));
+        ButtonList[3].onClick.AddListener(() => SceneManager.LoadScene(Stage + 4));
+        ButtonList[4].onClick.AddListener(() => SceneManager.LoadScene(Stage + 5));
+        // ButtonList[5].onClick.AddListener(() => SceneManager.LoadScene("AmidaPuzzle"));
     }
 
     private void OtherButtonSetting()
     {
         titleButton.onClick.AddListener(() => SceneManager.LoadScene("TitleScene"));
     }
+
 }
